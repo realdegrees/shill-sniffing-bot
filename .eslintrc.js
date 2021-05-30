9351c3d7498c5570b803eb44416a670fe2d27314
realdegrees <>
2021-05-30 20:37:48 +0200

    updates configs and packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,22 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: "./tsconfig.json"
+        project: './tsconfig.json'
     },
     plugins: [
         '@typescript-eslint',
     ],
     extends: [
         'airbnb-typescript/base'
-    ]
+    ],
+    rules: {
+        'no-console': 0,
+        'max-len': [1, {
+            'code': 140,
+            'ignoreComments': true,
+            'ignoreTrailingComments': true,
+            'ignoreUrls': true,
+            'ignoreStrings': true
+        }]
+    }
 };
